080429b95067a06b8a1f3484e0a1c38b1fcb4aeb
rohilario <rodrigohilariocomercial@gmail.com>
2024-01-14 00:50:08 -0300

    Fix: Swagger Doc error

--- Program.cs
+++ Program.cs
@@ -96,8 +96,17 @@ builder.Services.AddSwaggerGen(x =>
 
 builder.Services.AddSwaggerGen(c =>
 {
-c.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger - App .NET", Version = "v1" , Description = "API RESTful com .NET 7."
-    ,Contact = { Email = "mailto:rodrigohilariocomercial@gmail.com" }
+    c.SwaggerDoc
+        ("v1", new OpenApiInfo
+        {
+            Title = "Swagger - App .NET"
+            ,
+            Version = "v1"
+            ,
+            Description = "API RESTful com .NET 7."
+            ,
+            Contact = { Email = "mailto:rodrigohilariocomercial@gmail.com" }
+        });
 });
 
 const string logPath = "../log/serilog-dotnetwebapi.log";
